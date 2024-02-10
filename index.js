5b7b133dfc9d9ce6337d57a9b1c40a8c9f40bc2e
Suraj Gautam <surajgautam783792@gmail.com>
2024-02-10 22:15:57 +0530

    Loops

---
+++ index.js
@@ -0,0 +1,78 @@
+// ********For Loops-------------------------->>>>>>>>>>>>>>>>
+
+// for (let i = 0; i < 1000; i++) {
+//   console.log("Rohan");
+// }
+
+// e.g Calculate Sum of  1 to 9---------------------->>>>>>>>>>>>>>>>>>
+// let sum = 0;
+// for (let i = 0; i <= 1000; i++) {
+//   sum = sum + i;
+// }
+// console.log("sum", sum);
+
+// ---------------------->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+// // e.g  In Below It Represent The Blocked Scoped
+// for (let i = 0; i <= 5; i++) {
+//   console.log("i=", i);
+// }
+// console.log(i);
+
+// ---------------------->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+// e.g  In Below It Represent The Global Scoped Don't Do This
+// for (var i = 0; i <= 5; i++) {
+//   console.log("i=", i);
+// }
+// console.log(i);
+
+// ********Infinite Loops-------------------------->>>>>>>>>>>>>>>>
+// for (var i = 1; i >= 0; i++) {
+//   console.log("i=", i);
+// }
+// console.log(i);
+
+// ********While Loops-------------------------->>>>>>>>>>>>>>>>
+// let i = 1;
+// while (i <= 5) {
+// i++;
+//   console.log("i=", i);
+// }
+
+// Important :- Note While Loops Only Run When Condition Is True
+// let i = 20;
+// while (i <= 5) {
+// i++;
+//   console.log("i=", i);
+// }
+
+// ********Do While Loops-------------------------->>>>>>>>>>>>>>>>
+// Important :- Note Do-While Loops Can Run Atleast One Time When Condition Is False
+// let i = 9;
+// do {
+//   console.log("i=", i);
+//   i++;
+// } while (i <= 5);
+
+// ********For-of-loop-------------------------->>>>>>>>>>>>>>>>
+// Special Loops Using In Strings And Arrays
+// let str = "Udit";
+// for (let i of str) {
+//   console.log(i);
+// }
+
+// ********For-in-loop-------------------------->>>>>>>>>>>>>>>>
+// Special Loops Using In Objects And Arrays
+
+// let student = {
+//   name: "Manisha Kumari",
+//   role: "Full Stack Web Developer",
+//   age: 25,
+
+// };
+
+// for (let Key in student) {
+//   console.log("Key=", Key);
+// }
+
+// student[Key]) Use This To Know About The Value Of Name ,Role, Age
